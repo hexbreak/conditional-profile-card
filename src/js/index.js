91531d8a67cc65ed5f2de306be0c7b6e4bc0f18c
Rolando Scarfullery <74724935+hexbreak@users.noreply.github.com>
2020-12-13 00:32:04 -0500

    changes on variables

--- src/js/index.js
+++ src/js/index.js
@@ -26,23 +26,39 @@ function render(variables = {}) {
   console.log("These are the current variables: ", variables); //print on the console
   // here we ask the logical questions to make decisions on how to build the html
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
-  let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
+  let cover = `<div class="cover"><img src="https://mymodernmet.com/wp/wp-content/uploads/2019/12/stefano-gardel-japan-night-photography-16.jpg" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  if (variables.name == null) variables.name = "Rolando";
+  if (variables.lastname == null) variables.lastname = "Scarfullery";
+  if (variables.role == null) variables.role = "Student @ 4Geeks";
+  if (variables.country == null) variables.country = "U.S.A.";
+  if (variables.city == null) variables.city = "Fort Lauderdale";
+  if (variables.twitter == null)
+    variables.twitter = "https://twitter.com/rolandoscarfullery";
+  if (variables.github) variables.github = "https://github.com/hexbreak";
+  if (variables.linkedin == null)
+    variables.linkedin = "https://linkedin.com/rolscarfullery";
+  if (variables.instagram == null)
+    variables.instagram = "https://instagram.com/rolscarfullery";
+  if (variables.avatarURL)
+    variables.avatarURL = "https://i.imgur.com/6WavJXr.jpeg";
+  if (variables.socialMediaPosition == "position-left")
+    variables.socialMediaPosition = "position-right";
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
-          <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/alesanchezr"><i class="fa fa-twitter"></i></a></li>
-            <li><a href="https://github.com/alesanchezr"><i class="fa fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/alesanchezr"><i class="fa fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/alesanchezr"><i class="fa fa-instagram"></i></a></li>
-          </ul>
-        </div>
+       ${cover}
+     <img src="${variables.avatarURL}" class="photo" />
+     <h1>${variables.name} ${variables.lastname}</h1>
+     <h2>${variables.role}</h2>
+     <h3>${variables.city}, ${variables.country}</h3>
+     <ul class="position-right">
+       <li><a href="${variables.twitter}" target="_blank"><i class="fa fa-twitter"></i></a></li>
+       <li><a href="${variables.github}" target="_blank"><i class="fa fa-github"></i></a></li>
+       <li><a href="${variables.linkedin}" target="_blank"><i class="fa fa-linkedin"></i></a></li>
+       <li><a href="${variables.instagram}" target="_blank"><i class="fa fa-instagram"></i></a></li>
+      </ul>
+    </div>
     `;
 }
 
